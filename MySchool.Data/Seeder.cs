c314f4a7425034282ea8e985528d4c5ded7c31d3
Sodiq Alabi <alabisdq@gmail.com>
2023-07-01 09:14:52 +0100

    seed data to table

--- MySchool.Data/Seeder.cs
+++ MySchool.Data/Seeder.cs
@@ -2,6 +2,7 @@
 using MySchool.Data.Context;
 using MySchool.Models.Entities;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace MySchool.Data
 {
@@ -20,17 +21,16 @@ namespace MySchool.Data
                 await context.Database.EnsureCreatedAsync();
                 if (!context.Users.Any())
                 {
-                    string roles = File.ReadAllText(@"JsonFiles/Roles.json");
-                    List<IdentityRole> listOfRoles = JsonConvert.DeserializeObject<List<IdentityRole>>(roles);
-                    //List<string> listOfRoles = new List<string> { "Admin", "Regular" };
+                    string rolesJson = File.ReadAllText(@"JsonFiles/Roles.json");
+                    List<Role> listOfRoles = JsonConvert.DeserializeObject<List<Role>>(rolesJson);
 
                     foreach (var role in listOfRoles)
                     {
-                        await roleManager.CreateAsync(role);
+                        await roleManager.CreateAsync(new IdentityRole(role.Name));
                     }
 
-                    string users = File.ReadAllText(@"JsonFiles/Users.json");
-                    List<User> listOfUsers = JsonConvert.DeserializeObject<List<User>>(users);
+                    string usersJson = File.ReadAllText(@"JsonFiles/Users.json");
+                    List<User> listOfUsers = JsonConvert.DeserializeObject<List<User>>(usersJson);
                     int i = 0;
                     foreach (var user in listOfUsers)
                     {
@@ -46,6 +46,41 @@ namespace MySchool.Data
                             await userManager.AddToRoleAsync(user, "Regular");
                     }
                 }
+
+                // Add code to seed other tables using the corresponding JSON files
+                string classesJson = File.ReadAllText(@"JsonFiles/Classes.json");
+                List<Class> listOfClasses = JsonConvert.DeserializeObject<List<Class>>(classesJson);
+                context.Classes.AddRange(listOfClasses);
+
+                string quizzesJson = File.ReadAllText(@"JsonFiles/Quizzes.json");
+                List<Quiz> listOfQuizzes = JsonConvert.DeserializeObject<List<Quiz>>(quizzesJson);
+                context.Quizzes.AddRange(listOfQuizzes);
+
+                string schoolsJson = File.ReadAllText(@"JsonFiles/Schools.json");
+                List<School> listOfSchools = JsonConvert.DeserializeObject<List<School>>(schoolsJson);
+                context.Schools.AddRange(listOfSchools);
+
+                string studentsJson = File.ReadAllText(@"JsonFiles/Students.json");
+                List<Student> listOfStudents = JsonConvert.DeserializeObject<List<Student>>(studentsJson);
+                context.Students.AddRange(listOfStudents);
+
+                string subjectsJson = File.ReadAllText(@"JsonFiles/Subjects.json");
+                List<Subject> listOfSubjects = JsonConvert.DeserializeObject<List<Subject>>(subjectsJson);
+                context.Subjects.AddRange(listOfSubjects);
+
+                string teachersJson = File.ReadAllText(@"JsonFiles/Teachers.json");
+                List<Teacher> listOfTeachers = JsonConvert.DeserializeObject<List<Teacher>>(teachersJson);
+                context.Teachers.AddRange(listOfTeachers);
+
+                string teacherAssignmentsJson = File.ReadAllText(@"JsonFiles/TeacherAssignments.json");
+                List<TeacherAssignment> listOfTeacherAssignments = JsonConvert.DeserializeObject<List<TeacherAssignment>>(teacherAssignmentsJson);
+                context.TeacherAssignments.AddRange(listOfTeacherAssignments);
+
+                string topicsJson = File.ReadAllText(@"JsonFiles/Topics.json");
+                List<Topic> listOfTopics = JsonConvert.DeserializeObject<List<Topic>>(topicsJson);
+                context.Topics.AddRange(listOfTopics);
+
+                await context.SaveChangesAsync();
             }
             catch (Exception)
             {
--- MySchoolApp/Program.cs
+++ MySchoolApp/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MySchool.Data;
 using MySchool.Data.Context;
 using MySchool.Models.Entities;
 using MySchoolApp.Extensions;
 using Serilog;
 using Serilog.Extensions.Logging;
-
 
 public class Program
 {
@@ -15,15 +15,20 @@ public class Program
 
         // Add services to the container.
 
-        Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/MySchoolLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("log/MySchoolLogs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
         builder.Host.UseSerilog();
         builder.Services.AddControllers();
         builder.Services.AddDbContext<MySchoolDbContext>(options =>
-                        options.UseSqlite(builder.Configuration.GetConnectionString("Con")));
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            options.UseSqlite(builder.Configuration.GetConnectionString("Con")));
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<MySchoolDbContext>().AddDefaultTokenProviders();
+        builder.Services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<MySchoolDbContext>()
+            .AddDefaultTokenProviders();
         builder.Services.Configure<IdentityOptions>(options => options.User.RequireUniqueEmail = true);
         var loggerFactory = new SerilogLoggerFactory(Log.Logger);
         builder.Services.AddSingleton<ILoggerFactory>(loggerFactory);
@@ -32,6 +37,15 @@ public class Program
 
         var logger = app.Services.GetRequiredService<ILogger<Program>>();
         app.ConfigureExceptionHandler(logger);
+
+        // Seed the database
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<MySchoolDbContext>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            Seeder.SeedDataBase(roleManager, userManager, dbContext);
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -50,6 +64,5 @@ public class Program
         app.MapControllers();
 
         app.Run();
-
     }
 }
